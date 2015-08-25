4c82b2b7e4d7dfe5eb68d8570d063dde5531ee7d
Danny Berger <dpb587@gmail.com>
2015-08-25 00:12:49 -0600

    wip

--- ci/tasks/run-deployment-errand/run.sh
+++ ci/tasks/run-deployment-errand/run.sh
@@ -7,6 +7,6 @@ bosh -n target "${BOSH_TARGET}"
 bosh -n login "${BOSH_USER}" "${BOSH_PASSWORD}"
 
 bosh \
-  -d "${BOSH_DEPLOYMENT_FILE}" \
+  -d "${DEPLOYMENT_FILE}" \
   run errand \
   "${ERRAND_NAME}"
