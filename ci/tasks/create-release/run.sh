5fd4bccbca7ef15134a036f6d58a5f8dcd614500
Danny Berger <dpb587@gmail.com>
2015-08-25 01:00:22 -0600

    wip

--- ci/tasks/create-release/run.sh
+++ ci/tasks/create-release/run.sh
@@ -6,9 +6,14 @@ set -u
 cd "${PWD}/repo"
 
 EXTRA_OPTS="${EXTRA_OPTS:-}"
+FINAL_RELEASE="${FINAL_RELEASE:-false}"
+VERSION=$( cat ../version/number )
 
-if [[ "true" == "${FINAL_RELEASE:-}" ]] ; then
+if [[ "true" == "$FINAL_RELEASE" ]] ; then
   EXTRA_OPTS="${EXTRA_OPTS} --final"
+else
+  VERSION_BUILD=$( git rev-parse HEAD | cut -c -10 )
+  VERSION="${VERSION}+rev.${VERSION_BUILD}"
 fi
   
 bosh -n create release \
@@ -16,6 +21,17 @@ bosh -n create release \
   --version="$( cat ../version/number )" \
   --with-tarball
 
-[[ "true" == "${FINAL_RELEASE:-}" ]] || exit
+echo "${VERSION}" > VERSION
 
+if [[ "true" == "$FINAL_RELEASE" ]] ; then
+  exit
+fi
 
+git add -A .final_builds releases
+
+(
+  echo "Release v$VERSION"
+  echo ""
+  cat releases/logsearch-$VERSION.md
+) \
+  | git commit -F-
