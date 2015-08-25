d09de8143fe8f7195a9114f011a9381ec2cc83c7
Danny Berger <dpb587@gmail.com>
2015-08-25 15:34:32 -0600

    wip

--- ci/tasks/create-release/run.sh
+++ ci/tasks/create-release/run.sh
@@ -5,57 +5,21 @@ set -u
 
 VERSION=$( cat version/number )
 
-RELEASE_DIR="${RELEASE_DIR:-repo}"
+cd repo
 
-cd "$PWD/$RELEASE_DIR"
-
-EXTRA_OPTS="${EXTRA_OPTS:-}"
-FINAL_RELEASE="${FINAL_RELEASE:-false}"
-RELEASE_SRC_DIR="dev_releases-src/"
-
-if [[ "true" == "$FINAL_RELEASE" ]] ; then
-  EXTRA_OPTS="$EXTRA_OPTS --final"
-  RELEASE_SRC_DIR="releases-src/"
-fi
+#
+# create a dev release
+#
 
 bosh -n create release \
-  $EXTRA_OPTS \
   --version="$VERSION" \
   --with-tarball
-
-if [[ "true" == "$FINAL_RELEASE" ]] ; then
-  #
-  # create final release
-  #
-
-  git config user.email "${CI_EMAIL:-ci@localhost}"
-  git config user.name "${CI_NAME:-CI Bot}"
-
-  git add -A .final_builds releases
-
-  (
-    set -e
-    echo "Release v$VERSION"
-    echo ""
-    cat releases/logsearch-$VERSION.md
-  ) \
-    | git commit -F-
-
-
-  #
-  # write out some metadata
-  #
-
-  echo "v$VERSION" > ../name
-  cp releases/logsearch-$VERSION.md ../notes.md
-  git rev-parse HEAD > ../commit
-fi
 
 #
 # dump the source code
 #
 
-mkdir -p $RELEASE_SRC_DIR/logsearch
+mkdir -p dev_releases-src/logsearch
 
 COMMIT=$( git rev-parse HEAD )
 COMMIT_SHORT=$( echo $COMMIT | cut -c -10 )
@@ -64,4 +28,4 @@ git archive \
   --format=tar.gz \
   --prefix=logsearch-$COMMIT_SHORT/ \
   $COMMIT \
-  > $RELEASE_SRC_DIR/logsearch/logsearch-src-$VERSION.tgz
+  > dev_releases-src/logsearch/logsearch-src-$VERSION.tgz
---
+++ ci/tasks/finalize-release/run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+set -u
+
+VERSION=$( cat version/number )
+
+cd repo
+
+#
+# finalize the release
+#
+
+bosh -n finalize release \
+  ../release/tarball.tgz \
+  --version="$VERSION"
+
+bosh -n create release \
+  releases/logsearch/logsearch-$VERSION.yml \
+  --with-tarball
+
+#
+# commit final release
+#
+
+git config user.email "${CI_EMAIL:-ci@localhost}"
+git config user.name "${CI_NAME:-CI Bot}"
+
+git add -A .final_builds releases
+
+(
+  set -e
+  echo "Release v$VERSION"
+  echo ""
+  cat releases/logsearch-$VERSION.md
+) \
+  | git commit -F-
+
+#
+# write out some metadata
+#
+
+echo "v$VERSION" > ../name
+cp releases/logsearch-$VERSION.md ../notes.md
+git rev-parse HEAD > ../commit
+
+#
+# dump the source code
+#
+
+mkdir -p releases-src/logsearch
+
+COMMIT=$( git rev-parse HEAD )
+COMMIT_SHORT=$( echo $COMMIT | cut -c -10 )
+
+git archive \
+  --format=tar.gz \
+  --prefix=logsearch-$COMMIT_SHORT/ \
+  $COMMIT \
+  > releases-src/logsearch/logsearch-src-$VERSION.tgz
